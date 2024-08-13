1631b7f6d423e4dfa1379b13622b33ef2d49341e
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-08-14 00:47:18 +0500

    DNK-Clothing-Store

--- src/SingleUseComponent/Content2/Content2.jsx
+++ src/SingleUseComponent/Content2/Content2.jsx
@@ -1,6 +1,9 @@
 import { Box, Container, Grid } from "@mui/material"
 import css from './Content2.module.css'
 import Content2Card from "../../SharedComponent/Content2Card/Content2Card"
+import img1 from '../../../src/assets/Images/card2women.jpeg'
+import img2 from '../../../src/assets/Images/card2men.jpeg'
+import img3 from '../../../src/assets/Images/card2accesories.jpeg'
 const Content2=()=>{
     return(
         <>
@@ -13,9 +16,9 @@ const Content2=()=>{
                 <Box className={css.Box}><img src="../../../src/assets/Images/client4.jpeg" alt="" /></Box>
                 </Box><br />
                     <Grid container className={css.Grid_box} >
-                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc='../../../src/assets/Images/card2women.jpeg' title='20% Off On Tank Tops' btn='Shop Now'/></Grid>
-                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc='../../../src/assets/Images/card2men.jpeg' title='Latest Eyewear For You' btn='Shop Now'/></Grid>
-                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc='../../../src/assets/Images/card2accesories.jpeg' title="Let's Lorem Suit Up!" btn='Check Out'/></Grid>
+                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc={img1} title='20% Off On Tank Tops' btn='Shop Now'/></Grid>
+                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc={img2} title='Latest Eyewear For You' btn='Shop Now'/></Grid>
+                        <Grid item xs={12} sm={12} md={3.9} lg={3.9} xl={3.9} className={css.Grid_card}><Content2Card ImgSrc={img3} title="Let's Lorem Suit Up!" btn='Check Out'/></Grid>
                     </Grid>
             </Container>
         </Box>
